0d7c6328db652279d34cd2e84c048583296bfc4c
MichalKs <mxiezopolski@gmail.com>
2014-06-16 22:11:44 +0200

    Init

---
+++ mainwindow.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow) {
+
+    ui->setupUi(this);
+
+    connect(ui->cancelButton, SIGNAL(clicked()), this, SLOT(close()));
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
